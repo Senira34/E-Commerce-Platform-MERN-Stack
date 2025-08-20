9a1dbcc41d68ce5e35dc8fabb02ae2ff55f449e2
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-08-20 21:20:32 +0530

    Update productdisplay.jsx

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ProductDisplay.css'
-import star_icon from "../Assets/star_icon.png";
-import star_dull_icon from "../Assets/star_dull_icon.png";
+import star_icon from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
 
 const ProductDisplay = (props) => {
   const {product} = props;
@@ -30,6 +30,16 @@ const ProductDisplay = (props) => {
             <img src={star_icon} alt="" />
             <img src={star_dull_icon} alt="" />
             <p>(122)</p>
+          </div>
+
+          <div className="productdisplay-right-prices">
+            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            <div className="productdisplay-right-price-new">${product.new_price}</div>
+
+          </div>
+
+          <div className="productdisplay-right-description">
+            
           </div>
             
         </div>
