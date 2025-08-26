8c179138dc744c1c6a5d5f88fc3a509a018815e9
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-08-26 23:43:20 +0530

    Update DescriptionBox.jsx

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './DescriptionBox.css'
 
 const DescriptionBox = () => {
   return (
-    <div>DescriptionBox</div>
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci non repellat perferendis possimus repudiandae minima quos error animi distinctio, quibusdam sint necessitatibus eligendi quas in nam rem ea impedit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, consectetur optio! Dicta amet aliquam, cum perspiciatis molestiae odit mollitia culpa tempore perferendis non excepturi deserunt adipisci eaque temporibus ullam rerum!</p>
+        </div>
+
+    </div>
   )
 }
 
