89190579b644674c66870a7958506767e68529c4
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-08-28 01:20:10 +0530

    Update Shop Context.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -5,7 +5,7 @@ import all_product from '../Components/Assets/all_product'
 export const ShopContext = createContext(null);
 const getDefaultCart = ()=> {
         let cart ={};
-        for (let index = 0; index < all_product.length; index++){
+        for (let index = 0; index < all_product.length+1; index++){
             cart[index] = 0;
 
         }
@@ -14,12 +14,20 @@ const getDefaultCart = ()=> {
 
 const ShopContextProvider = (props) => {
     const [cartItems,setCartItems] = useState(getDefaultCart());
-    const contextValue = {all_product,cartItems};
+    
+    const addToCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
+    }
 
-    console.log
-   
+    const removeFromCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
 
     
+   
+
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
 
     return(
         <ShopContext.Provider value={contextValue}>
