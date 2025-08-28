86f232e15ce0d1ad7a7b1be58fccb904715dc32f
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-08-28 23:59:51 +0530

    Error resolve

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 
 const CartItems = () => {
-    const {all_product,CartItems,removeFromCart} = useContext(ShopContext);
+    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -19,15 +19,15 @@ const CartItems = () => {
         </div>
         <hr />
         {all_product.map((e)=>{
-        if(CartItems[e.id]>0)
+        if(cartItems[e.id]>0)
         {
             return <div>
         <div className="cartitems-format">
             <img src={e.image} alt="" className='carticon-product-icon'/>
             <p>{e.name}</p>
             <p>${e.new_price}</p>
-            <button className='cartitems-quantity'>{CartItems[e.id]}</button>
-            <p>{e.new_price*CartItems[e.id]}</p>
+            <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+            <p>{e.new_price*cartItems[e.id]}</p>
             <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
         </div>
         <hr />
