ce82880fbaf82c15de73a30deb9c8cfdf16ddcff
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-08-29 16:05:40 +0530

    Change and update Shopcontext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -28,10 +28,11 @@ const ShopContextProvider = (props) => {
     
    const getTotalCartAmount = () => {
         let totalAmount = 0;
-        for(const item in cartItems){
+        for(const item in cartItems)
+        {
             if(cartItems[item]>0)
             {
-                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                let itemInfo = all_product.find((product)=>product.id===Number(item));
                 totalAmount += itemInfo.new_price * cartItems[item];
             
             }   
@@ -40,7 +41,19 @@ const ShopContextProvider = (props) => {
         }
    }
 
-    const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+   const getTotalCartItems = () => {
+    let totalItem = 0;
+    for(const item in cartItems)
+    {
+        if(cartItems[item]>0)
+        {
+            totalItem+= cartItems[item];
+        }
+    }
+    return totalItem;
+   }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
 
     return(
         <ShopContext.Provider value={contextValue}>
