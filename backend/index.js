30271f491b8542300e6afa548cc09bb53840802a
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-09-05 13:30:19 +0530

    Add Product schema and endpoint
    Introduced a Mongoose model for products and added a POST /addproduct endpoint to create and save new products to the database.

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -42,6 +43,60 @@ app.post("/upload",upload.single('product'),(req,res)=>{
         success:1,
         image_url:`http://localhost:${port}/images/${req.file.filename}`
     })
+
+})
+
+// Schema for Creating Products
+
+const product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+
+    },
+    name:{
+        type: String,
+        required: true, 
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true, 
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    },
+})
+
+app.post('/addproduct',async(req,res)=>{
+    const product = new product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+
+    console.log(product);
+    await product.save();
+    console.log("Saved");
 
 })
 
