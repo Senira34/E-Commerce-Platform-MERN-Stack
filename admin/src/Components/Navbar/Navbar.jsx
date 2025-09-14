5394430c106bb46d38abe2208d67456cff85ece4
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-09-15 00:49:06 +0530

    Add Navbar styles and fix asset imports

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import nav_logo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/navProfile.svg'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={nav_logo} alt="" className="nav-logo" />
-        <img src={navProfile} alt="Profile" className="nav-profile" />
+        <img src={navlogo} alt="" className="nav-logo" />
+        <img src={navProfile} alt="" className="nav-profile" />
 
     </div>
   )
